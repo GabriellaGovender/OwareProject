b63496d2aadd95f14004b35f35001b7c87abad74
Gabriella Govender <gabriella.govender@gmail.com>
2021-05-18 11:02:40 +0200

    Oware_tests

--- Oware.Test/UnitTest1.cs
+++ Oware.Test/UnitTest1.cs
@@ -1,9 +1,12 @@
 using NUnit.Framework;
 
+
 namespace Oware.Test
 {
     public class Tests
     {
+
+
         [SetUp]
         public void Setup()
         {
@@ -12,7 +15,32 @@ namespace Oware.Test
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //ARRANGE
+            House h = new House(1,1);
+            int intialSeeds = h.GetCount();
+            
+            //ACT
+            h.ResetHouse();
+            int testlSeeds = h.GetCount();
+            //ASSERT
+            Assert.That(testlSeeds, Is.EqualTo(intialSeeds));
+        
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //ARRANGE
+            ScoreHouse sub = new ScoreHouse();
+            Player p = new Player("test");
+            Seed s = new Seed();
+            int result = p.GetScore() + 1;
+            //ACT
+            p.AddSeedToScoreHouse(s);
+            int test = p.GetScore();
+            //ASSERT
+            Assert.That(test, Is.EqualTo(result));
+        
         }
     }
 }
